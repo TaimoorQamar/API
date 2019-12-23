e8c2cc71a31b5f995960cc6946c9a1b084f323f5
Taimoor Qamar <taimoorqamar1122@gmail.com>
2019-12-23 13:23:57 +0500

    Created Model -a

--- database/migrations/2019_12_20_131917_create_reviews_table.php
+++ database/migrations/2019_12_20_131917_create_reviews_table.php
@@ -15,6 +15,14 @@ class CreateReviewsTable extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('product_id')->unsigned();
+            $table->foreign('product_id')
+            ->refrences('id')
+            ->on('products')
+            ->onDelete('cascade');
+            $table->string('customer');
+            $table->text('review');
+            $table->integer('star');
             $table->timestamps();
         });
     }
